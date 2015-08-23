9ab0fca755eb761b6724b6f55a6ec82d2bd1af31
Steve Ashton <nothsaevets@gmail.com>
2015-08-22 22:21:00 -0400

    Import emacs config

---
+++ custom.el
@@ -0,0 +1,19 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(coffee-tab-width 2))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(rainbow-delimiters-depth-2-face ((t (:foreground "#01fd7e"))))
+ '(rainbow-delimiters-depth-3-face ((t (:foreground "DodgerBlue2"))))
+ '(rainbow-delimiters-depth-4-face ((t (:foreground "orange"))))
+ '(rainbow-delimiters-depth-5-face ((t (:foreground "#fd01fd"))))
+ '(rainbow-delimiters-depth-6-face ((t (:foreground "PaleGreen1"))))
+ '(rainbow-delimiters-depth-7-face ((t (:foreground "#962ef7"))))
+ '(rainbow-delimiters-depth-8-face ((t (:foreground "OliveDrab3"))))
+ '(rainbow-delimiters-depth-9-face ((t (:foreground "cyan")))))
